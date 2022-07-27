de36737500e471bbee6976306d6834aa5ce7a4db
Tiziana <tizianaamicarella.tag@gmail.com>
2022-07-27 07:18:25 -0400

    Popoular sites card

---
+++ src/components/SiteCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const SiteCard = ({ sitesList }) => {
+  return (
+    <div className="container border-2 border-rose">
+      {sitesList.map((site) => {
+        return (
+          <div
+            key={site.name}
+            className="border-2 border-black mb-4 rounded w-96"
+          >
+            <a href={site.url} target="_blank">
+              <img src={site.image} alt={site.name} />
+            </a>
+            <p>{site.name}</p>
+            <p>{site.description}</p>
+            <p>{site.category}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SiteCard;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const sitesList = [
+  {
+    name: "KFC",
+    url: "https://www.google.com/",
+    description:
+      "KFC is a fast-food restaurant chain headquartered in the United States. It is the largest restaurant chain in the United States, and the largest in the world.",
+    image:
+      "https://images.unsplash.com/photo-1657299170932-e75407883a6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    category: "Fast Food",
+  },
+  {
+    name: "Burger King",
+    url: "https://www.google.com/",
+    description:
+      "Burger King is a fast food restaurant chain headquartered in the United States. It is the largest restaurant chain in the United States, and the largest in the world.",
+    image:
+      "https://images.unsplash.com/photo-1657299156537-f4bcdced5392?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    category: "Fast Food",
+  },
+  {
+    name: "McDonald's",
+    url: "https://www.google.com/",
+    description:
+      "McDonald's is a fast food restaurant chain headquartered in the United States. It is the largest restaurant chain in the United States, and the largest in the world.",
+    image:
+      "https://images.unsplash.com/photo-1657299170207-d6df52b27811?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    category: "Fast Food",
+  },
+];
