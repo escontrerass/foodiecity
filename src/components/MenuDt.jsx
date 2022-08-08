78b86db4f552145437d1a0e9d5e0055fd9b533e9
escontrerass <edgarsamuel.contreras@unet.edu.ve>
2022-08-08 11:04:40 -0400

    Fix desktop login

--- src/components/MenuDt.jsx
+++ src/components/MenuDt.jsx
@@ -8,7 +8,8 @@ import { PrimaryButton } from './PrimaryButton';
 import { FaSearch } from 'react-icons/fa';
 
 export const MenuDt = () => {
-  const { setLogin, setSignIn } = React.useContext(menuContext);
+  const { setLogin, setSignIn, setBusiness, setStatus, status } =
+    React.useContext(menuContext);
   const [searchText, setSearchText] = React.useState('');
   const navigate = useNavigate();
   const handleSearchText = (text) => setSearchText(text);
@@ -19,34 +20,67 @@ export const MenuDt = () => {
   const handleSignIn = () => {
     setSignIn(true);
   };
+  const handleBusiness = () => {
+    setBusiness(true);
+  };
+  const handleLogout = () => setStatus(!status);
 
   return (
     <header className='flex flex-col gap-3 fixed top-0 w-full bg-gradient-to-b from-amber-200 bg-white z-50'>
-      <nav className='flex justify-between items-center pt-5 px-16'>
+      <nav className='flex justify-between items-center pt-5 px-8'>
         <button className='w-32'>
           <Link to='/'>
             <Logo status={true} />
           </Link>
         </button>
-        <div className='flex items-center gap-4 font-bold w-9/12'>
-          <Link to='/'>
+        <div className='flex items-center gap-2 font-bold w-9/12 justify-end'>
+          <Link to='/' className='hover:text-red'>
             <p className='whitespace-nowrap'>Inicio</p>
           </Link>
-          <Link to='/category'>
+          <Link to='/category' className='hover:text-red'>
             <p className='whitespace-nowrap'>Categorias</p>
           </Link>
-          <Link to='/news'>
+          <Link to='/news' className='hover:text-red'>
             <p className='whitespace-nowrap'>Sitios nuevos</p>
           </Link>
-          <Link to='/popular'>
+          <Link to='/popular' className='hover:text-red'>
             <p className='whitespace-nowrap'>Sitios populares</p>
           </Link>
-          <SecondaryButton
-            title='Iniciar sesion'
-            font={true}
-            handle={handleLogin}
-          />
-          <PrimaryButton title='Registrar' font={true} handle={handleSignIn} />
+          {!status && (
+            <div className='w-40'>
+              <SecondaryButton
+                title='Iniciar sesion'
+                font={true}
+                handle={handleLogin}
+              />
+            </div>
+          )}
+          {!status && (
+            <div className='w-40'>
+              <PrimaryButton
+                title='Registrar'
+                font={true}
+                handle={handleSignIn}
+              />
+            </div>
+          )}
+          {status && (
+            <div className='flex gap-2 items-center'>
+              <SecondaryButton
+                title='Agregar negocio'
+                font={true}
+                handle={handleBusiness}
+              />
+              <div className='flex items-center gap-3'>example@email.com</div>
+              <button
+                onClick={() => handleLogout()}
+                className='my-auto text-base text-red hover:text-black'
+                type='button'
+              >
+                Logout
+              </button>
+            </div>
+          )}
         </div>
       </nav>
       <div className='flex items-center w-full bg-lightGray h-20'>
